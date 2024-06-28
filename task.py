4ed19e5260fd0fe20dbbb9011ac117f96a4da88b
Anandhakrishnan K A <anandhu9287537233@gmail.com>
2024-06-28 16:51:24 +0530

    Intial commit

---
+++ task.py
@@ -0,0 +1,16 @@
+def pattern(rows,cols):
+  top=" _    "*(cols)
+  middle="/   \\_"*(cols-1)+"/   \\"
+  down="\\_/   "*(cols)
+  top1=" _   "
+
+
+
+  for i in range(rows):
+       if i==0:
+         print(top)
+       print(middle)
+       print(down)
+rows=int(input("enter the number of rows :"))
+columns=int(input("enter the number of coloums :"))
+pattern(rows,columns)
